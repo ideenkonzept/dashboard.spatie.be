83e566c2be841255194ec437b3376e2b02aa4fb3
Ahmed M Dabak <ahmed-dbak@hotmail.com>
2018-05-28 11:30:51 +0200

    wip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
 
 @javascript(compact('pusherKey', 'pusherCluster', 'usingNodeServer'))
 
-<dashboard id="dashboard" columns="5" rows="3">
+<dashboard id="dashboard" columns="6" rows="4">
     <twitter :initial-tweets="{{ json_encode($initialTweets) }}" position="a1:a3"></twitter>
     <uptime position="a1:a3"></uptime>
     <packagist position="b1"></packagist>
@@ -16,7 +16,7 @@
     <tasks team-member="freek" position="c3"></tasks>
     <tasks team-member="seb" position="d2"></tasks>
     <tasks team-member="willem" position="d3"></tasks>
-    <time-weather position="e1" date-format="ddd DD/MM" time-zone="Europe/Brussels" weather-city="Antwerp"></time-weather>
+    <time-weather position="e1" date-format="ddd DD/MM" time-zone="Europe/Berlin" weather-city="Wesel"></time-weather>
     <calendar position="e2:e3"></calendar>
     <internet-connection></internet-connection>
 </dashboard>
